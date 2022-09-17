c75d819c1198b78fd09f19d268ba6f4716abed0d
beingvirus <76661220+beingvirus@users.noreply.github.com>
2022-09-18 00:13:31 +0530

    init

--- action.sh
+++ action.sh
@@ -5,5 +5,4 @@ cd /root/TeamUltroid
 pip3 install -U -r req* --ignore-installed
 pip3 install -U -r res*/sta*/opt* --ignore-installed
 pip3 install hiredis redis
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
-docker run --privileged --env-file .env --rm -i ultroid
+bash startup
