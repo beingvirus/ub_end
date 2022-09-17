db40dcd6d20e111943bf751b85ed994fa035326f
beingvirus <76661220+beingvirus@users.noreply.github.com>
2022-09-18 00:08:34 +0530

    init

--- action.sh
+++ action.sh
@@ -2,5 +2,8 @@ branch=main
 git clone -b $branch https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
 cp ultroid/.env /root/TeamUltroid/.env
 cd /root/TeamUltroid
+pip3 install -U -r req* --ignore-installed
+pip3 install -U -r res*/sta*/opt* --ignore-installed
+pip3 install hiredis redis
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
 docker run --privileged --env-file .env --rm -i ultroid
